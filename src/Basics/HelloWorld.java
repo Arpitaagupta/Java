40890a19c8234aadcd0840f2d9b26f5dc85044cb
Arpita Gupta <arpitagupta6620@gmail.com>
2023-01-24 15:33:39 +0530

    Java

---
+++ src/Basics/HelloWorld.java
@@ -0,0 +1,15 @@
+package Basics;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world from 4th sem DS");   //to print
+    }
+}
+
+//single line comment
+
+    /*multi-line
+        comment
+     */
+
+//class is a keyword, so it cannot be name of any class.
